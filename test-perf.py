3f87e7467e173f88021875c528bec0d82733c741
Sang-Kil Park <kaon.park@kakaocorp.com>
2018-05-13 17:36:02 +0900

    Add Cython w/ class support.

--- test-perf.py
+++ test-perf.py
@@ -4,8 +4,10 @@ import timeit
 import math
 import pandas as pd
 from matplotlib import pyplot as plt
+import seaborn as sns
 import std
 import stdcy
+import stdcyc
 import stdpy
 
 
@@ -20,47 +22,50 @@ def standard_deviation(lst):
 
 
 if __name__ == '__main__':
-    start = 100; end = 1000; step = 10; include_pure_py = True
-    # start = 100; end = 50000; step = 2000; include_pure_py = False
-    lens = range(start, end, step)
+    # start = 1; end = 1000; step = 10; include_pure_py = True
+    start = 1; end = 100000; step = 2000; include_pure_py = False
+    lens = range(start, end + 1, step)
+
     py_time = []
     np_time = []
     c_time = []
     cy_time = []
+    cyc_time = []
     pybind_time = []
 
     for l in lens:
         rands = [random.random() for _ in range(0, l)]
+
         numpy_rands = np.array(rands)  # If you don't do this, It will convert every time
                                        # and there will be a some performance issues.
+        cyc_rands = stdcyc.pystd(rands)
+
+        np_time = np.append(np_time, timeit.timeit(lambda: np.std(numpy_rands), number=100))
+        c_time = np.append(c_time, timeit.timeit(lambda: std.standard_dev(rands), number=100))
+        cy_time = np.append(cy_time, timeit.timeit(lambda: stdcy.standard_dev(rands), number=100))
+        cyc_time = np.append(cyc_time, timeit.timeit(lambda: cyc_rands.standard_dev(), number=100))
+        pybind_time = np.append(pybind_time, timeit.timeit(lambda: stdpy.standard_dev(rands), number=100))
 
         if include_pure_py:
-            py_time = np.append(py_time, timeit.timeit(lambda: standard_deviation(rands), number=1000))
-            np_time = np.append(np_time, timeit.timeit(lambda: np.std(numpy_rands), number=1000))
-            c_time = np.append(c_time, timeit.timeit(lambda: std.standard_dev(rands), number=1000))
-            cy_time = np.append(cy_time, timeit.timeit(lambda: stdcy.standard_dev(rands), number=1000))
-            pybind_time = np.append(pybind_time, timeit.timeit(lambda: stdpy.standard_dev(rands), number=1000))
+            py_time = np.append(py_time, timeit.timeit(lambda: standard_deviation(rands), number=100))
 
-            data = np.array([np.transpose(py_time), np.transpose(np_time),
-                             np.transpose(c_time), np.transpose(cy_time), np.transpose(pybind_time)])
+            data = np.array([np.transpose(np_time), np.transpose(c_time), np.transpose(cy_time),
+                             np.transpose(cyc_time), np.transpose(pybind_time), np.transpose(py_time)])
         else:
-            np_time = np.append(np_time, timeit.timeit(lambda: np.std(numpy_rands), number=1000))
-            c_time = np.append(c_time, timeit.timeit(lambda: std.standard_dev(rands), number=1000))
-            cy_time = np.append(cy_time, timeit.timeit(lambda: stdcy.standard_dev(rands), number=1000))
-            pybind_time = np.append(pybind_time, timeit.timeit(lambda: stdpy.standard_dev(rands), number=1000))
-
-            data = np.array([np.transpose(np_time),
-                             np.transpose(c_time), np.transpose(cy_time), np.transpose(pybind_time)])
+            data = np.array([np.transpose(np_time), np.transpose(c_time), np.transpose(cy_time),
+                             np.transpose(cyc_time), np.transpose(pybind_time)])
 
     if include_pure_py:
-        df = pd.DataFrame(data.transpose(), index=lens, columns=['Python', 'Numpy', 'C++', 'Cython', 'Pybind11'])
+        df = pd.DataFrame(data.transpose(), index=lens, columns=['Numpy', 'C++', 'Cython', 'Cython w/ class',
+                                                                 'pybind11', 'Python'])
     else:
-        df = pd.DataFrame(data.transpose(), index=lens, columns=['Numpy', 'C++', 'Cython', 'Pybind11'])
+        df = pd.DataFrame(data.transpose(), index=lens, columns=['Numpy', 'C++', 'Cython', 'Cython w/ class',
+                                                                 'pybind11'])
 
     plt.figure()
     df.plot()
     plt.legend(loc='best')
     plt.ylabel('Time (Seconds)')
     plt.xlabel('Number of Elements')
-    plt.title('1k Runs of Standard Deviation')
+    plt.title('100 Runs of Standard Deviation')
     plt.show()
--- test-val.py
+++ test-val.py
@@ -1,5 +1,6 @@
 import std
 import stdcy
+import stdcyc
 import stdpy
 import numpy as np
 
@@ -7,8 +8,10 @@ import random
 
 random.seed(100)
 rands = [random.random() for _ in range(0, 5)]
+cyc_rands = stdcyc.pystd(rands)
 
 print('std: ', std.standard_dev(rands))
 print('stdcy: ', stdcy.standard_dev(rands))
+print('stdcyc: ', cyc_rands.standard_dev())
 print('stdpy: ', stdpy.standard_dev(rands))
 print('numpy: ', np.std(rands))
